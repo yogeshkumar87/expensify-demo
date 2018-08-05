deb42dea6a2fd17c0639c301f7333ffa21a36884
yogesh kumar <yogeshkumar87@hotmail.com>
2018-08-05 10:07:34 +0530

    added fetching logic

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -49,3 +49,29 @@ export const editExpense = (id, updates) => ({
     id,
     updates
 });
+
+// SET EXPENSES
+export const setExpenses = expenses => ({
+    type: 'SET_EXPENSES',
+    expenses
+});
+
+export const startSetExpenses = () => {
+    return dispatch => {
+        return database
+            .ref('expenses')
+            .once('value')
+            .then(snapshot => {
+                let expenses = [];
+
+                snapshot.forEach(childSnapshot => {
+                    expenses.push({
+                        id: childSnapshot.key,
+                        ...childSnapshot.val()
+                    });
+                });
+
+                dispatch(setExpenses(expenses));
+            });
+    };
+};
--- src/app.js
+++ src/app.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+import { startSetExpenses } from './actions/expenses';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 import 'react-dates/lib/css/_datepicker.css';
@@ -18,4 +16,8 @@ const jsx = (
     </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>...loading</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
